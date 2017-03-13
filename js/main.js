87f541838b16206c01bb7e942bbd85d248baf557
rdak <seva@chebykin.com>
2017-03-13 21:39:37 +0700

    v 9.3

--- js/main.js
+++ js/main.js
@@ -13379,56 +13379,6 @@ var images = [
 
 $(document).ready(function() {
     preloadPictures(loadingCounter);
-
-    var href = document.location.pathname;
-    var body = document.body;
-    if (href == "/"){
-        href = href + 'index';
-    }
-    else{
-        href = href.replace('.html', '');
-    }
-    href = href.replace('/', '');
-
-    switch(href){
-        case 'index':
-            var bg_image_style = colourList_main[0];
-            break;
-        case 'service_list':
-            var bg_image_style = colourList_service[0];
-            break;
-        default:
-            var bg_image_style = '#fff';
-            break;
-    }
-
-    var state = {'page_id': href, indexSlider : 0, bg_image_style : bg_image_style, mainslider : null};
-    var title = href;
-    var url = href + '.html';
-
-    history.pushState(state, title, url);
-    body.classList.add("page-" + href);
-
-    switch(state.page_id) {
-        case 'index':
-            initSlider(0);
-            break;
-        case 'service_list':
-            initSlider(0);
-            break;
-        case 'blog':
-        case 'success':
-        case 'service_page':
-            articleListPageInit();
-            break;
-        case 'about' :
-            break;
-        case 'article' :
-            
-        case 'contacts' :
-            checkContacts();
-            break;
-    }
 });
 
 var loadingCounter = function(){
@@ -13453,7 +13403,57 @@ var loadingCounter = function(){
                 setTimeout(function(){
                     $('.preloader-page').fadeOut();
                     $('#page-wrap').fadeIn();
+                    var href = document.location.pathname;
+                    var body = document.body;
+                    if (href == "/"){
+                        href = href + 'index';
+                    }
+                    else{
+                        href = href.replace('.html', '');
+                    }
+                    href = href.replace('/', '');
+
+                    switch(href){
+                        case 'index':
+                            var bg_image_style = colourList_main[0];
+                            break;
+                        case 'service_list':
+                            var bg_image_style = colourList_service[0];
+                            break;
+                        default:
+                            var bg_image_style = '#fff';
+                            break;
+                    }
+
+                    var state = {'page_id': href, indexSlider : 0, bg_image_style : bg_image_style, mainslider : null};
+                    var title = href;
+                    var url = href + '.html';
+
+                    history.pushState(state, title, url);
+                    body.classList.add("page-" + href);
+
+                    switch(state.page_id) {
+                        case 'index':
+                            initSlider(0);
+                            break;
+                        case 'service_list':
+                            initSlider(0);
+                            break;
+                        case 'blog':
+                        case 'success':
+                        case 'service_page':
+                            articleListPageInit();
+                            break;
+                        case 'about' :
+                            break;
+                        case 'article' :
+                            
+                        case 'contacts' :
+                            checkContacts();
+                            break;
+                    }
                 }, 600);
+
             }
         }
     , 20);
@@ -13900,21 +13900,11 @@ $(document).on('click', '.js-switchSlide', function(e){
             e.preventDefault();
             initSlider(index-1);
         }
-        else {
-            // e.preventDefault();
-        }
     }
 });
 
 function initSlider(currentIndex){
     var sliderList = $('.slider_list');
-    /*if (sliderList.length && $(window).width()>768){
-        $('body').addClass('slider_page');
-    }
-    else{
-        $('body').removeClass('slider_page');
-    }*/
-
     switchSlider(sliderList, currentIndex);
 
     var length = sliderList.find('.slider_bg_list .bg').length;
